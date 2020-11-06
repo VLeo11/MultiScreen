ddd296a57f6fda808cbdaf78602c528c5236f1b8
vleo11 <leontevvova20@gmail.com>
2020-11-06 15:10:31 +0300

    first

---
+++ app/src/main/java/com/example/multiscreen/Maldivi.java
@@ -0,0 +1,16 @@
+package com.example.multiscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.multiscreen.R;
+
+public class Maldivi extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maldivi);
+    }
+}
